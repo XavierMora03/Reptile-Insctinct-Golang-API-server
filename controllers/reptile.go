7e6caa3caf1c2a18955c2d128197505f255db2f0
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-09-04 18:54:36 -0600

    refactored all previously added, and the grom.io seems not working, cannot go get grom.io/grom, is weird

--- controllers/reptile.go
+++ controllers/reptile.go
@@ -1,1 +1,34 @@
 package controllers
+
+import (
+	"ReptileApi/models"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+var ReptilList = []models.Reptile{
+	{Name: "Mohave", RegularPrice: 2500, Price: 1999, AgeCategory: "Cria", Description: "Bonita mohave pastel", Genre: "Hembra"},
+	{Name: "Leopard Het-Clown", RegularPrice: 2000, Price: 1000, AgeCategory: "Cria", Description: "Leopard", Genre: "Macho"},
+	{Name: "Spotnose probable Blade 100% Het-Clown", RegularPrice: 9500, Price: 9200, AgeCategory: "Cria", Description: "Bonita Leopard Het-Clown", Genre: "Hembra"},
+	{Name: "Super Enchi Orange Dream Pastel ", RegularPrice: 2500, Price: 1999, AgeCategory: "Cria", Description: "Bonita Het Clown", Genre: "Hembra"},
+}
+
+func GetReptiles(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, ReptilList)
+}
+
+func DeleteReptiles(c *gin.Context) {
+	// var id_delete uint64 = c.GetHeader()
+}
+
+func PostReptiles(c *gin.Context) {
+	var newAlbum models.Reptile
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		log.Fatal(err)
+		return
+	}
+	ReptilList = append(ReptilList, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
--- models/reptile.go
+++ models/reptile.go
@@ -1,18 +1,6 @@
 package models
 
 // const MAX_PHOTOS_PER_REPTILE_PRODUCT int = 10
-import (
-	"github.com/Rhymond/go-money"
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
-)
-
-func trashFUnciont() {
-	a := money.New(100, money.MXN)
-	log.Fatal(a)
-}
-
 type Reptile struct {
 	ID           uint64 `json:"id"`
 	Name         string `json:"name"`
@@ -25,30 +13,4 @@ type Reptile struct {
 }
 
 type ReptilePictures struct {
-}
-
-var ReptilList = []Reptile{
-	{Name: "Mohave", RegularPrice: 2500, Price: 1999, AgeCategory: "Cria", Description: "Bonita mohave pastel", Genre: "Hembra"},
-	{Name: "Leopard Het-Clown", RegularPrice: 2000, Price: 1000, AgeCategory: "Cria", Description: "Leopard", Genre: "Macho"},
-	{Name: "Spotnose probable Blade 100% Het-Clown", RegularPrice: 9500, Price: 9200, AgeCategory: "Cria", Description: "Bonita Leopard Het-Clown", Genre: "Hembra"},
-	{Name: "Super Enchi Orange Dream Pastel ", RegularPrice: 2500, Price: 1999, AgeCategory: "Cria", Description: "Bonita Het Clown", Genre: "Hembra"},
-}
-
-func GetReptiles(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, ReptilList)
-}
-
-func DeleteReptiles(c *gin.Context) {
-	// var id_delete uint64 = c.GetHeader()
-}
-
-func PostReptiles(c *gin.Context) {
-	var newAlbum Reptile
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		log.Fatal(err)
-		return
-	}
-	ReptilList = append(ReptilList, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
