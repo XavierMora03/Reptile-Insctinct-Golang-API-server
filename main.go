8d702344df951bef068a18ba57c4929bd1be87b3
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-09-05 13:32:10 -0600

    deleted the database conected message

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"ReptileApi/controllers"
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	controllers.ConnectDatabase()
 
-	fmt.Print("SERVER RUNNING")
 	router := gin.Default()
 	router.GET("/reptiles", controllers.GetReptiles)
 	router.POST("/reptiles", controllers.PostReptiles)
